d40bc59e6909f5ed270c5bfb743502d4c91c703c
elizabeth-comstock <36870168+elizabeth-comstock@users.noreply.github.com>
2018-04-12 17:41:25 +0800

    Add files via upload

--- carControl.cpp
+++ carControl.cpp
@@ -57,26 +57,43 @@ int main(int argc, char *argv[]){
 
     cout << "Testing the GPIO Pins" << endl;
 
+    // Defines an enum for throttle statuses
+    enum ThrottleStatus {
+	tReverse = 0, 
+	tNeutral,
+	tForward
+    } ;
+
     // The names tell you how to connect the GPIO pins from the TX2 to the Arduino
     // toThrottleButton, for example, corresponds with throttleButton on the Arduino code
-    TX2_GPIO toThrottleButton = gpio300 ;
-    TX2_GPIO toLeftButton     = gpio301 ;
-    TX2_GPIO toRightButton    = gpio302 ;
+    TX2_GPIO toLeftButton    = gpio301 ;
+    TX2_GPIO toRightButton   = gpio302 ;
+    TX2_GPIO toForwardButton = gpio300 ;
+    TX2_GPIO toReverseButton = gpio299 ;
+    
     // Make the button and led available in user space
-    gpio_export(toThrottleButton) ;
     gpio_export(toLeftButton) ;
     gpio_export(toRightButton) ;
-    gpio_set_direction(toThrottleButton,GPIO_DIRECTION_OUTPUT) ;
+    gpio_export(toForwardButton) ;
+    gpio_export(toReverseButton) ;
+
     gpio_set_direction(toLeftButton,GPIO_DIRECTION_OUTPUT) ;
     gpio_set_direction(toRightButton,GPIO_DIRECTION_OUTPUT) ;
+    gpio_set_direction(toForwardButton,GPIO_DIRECTION_OUTPUT) ;
+    gpio_set_direction(toReverseButton,GPIO_DIRECTION_OUTPUT) ;
 
     // Wait for the push button to be pressed
-    cout << "Please mash WASD! ESC key quits the program" << endl;
+    cout << "Please mash WASD! ESC key quits the program" << endl ;
 
-    gpio_set_value(toThrottleButton,GPIO_PIN_VALUE_HIGH) ;
     gpio_set_value(toLeftButton,GPIO_PIN_VALUE_HIGH) ;
     gpio_set_value(toRightButton,GPIO_PIN_VALUE_HIGH) ;
-    char keyValue = 's';
+    gpio_set_value(toForwardButton,GPIO_PIN_VALUE_HIGH) ;
+    gpio_set_value(toReverseButton,GPIO_PIN_VALUE_HIGH) ;
+
+    char keyValue = 'p';
+    // Creates an enum to record the throttle status, and sets it to neutral
+    ThrottleStatus throttleStatus = tNeutral ;
+
     for( ; keyValue != 27 ; ) {
 	
 	keyValue = getkey();
@@ -86,11 +103,13 @@ int main(int argc, char *argv[]){
 	    gpio_set_value(toLeftButton,GPIO_PIN_VALUE_LOW) ;
 	    gpio_set_value(toRightButton,GPIO_PIN_VALUE_HIGH) ;
 	}
+
 	// Press d to turn right
 	else if(keyValue == 'd') {
 	    gpio_set_value(toLeftButton,GPIO_PIN_VALUE_HIGH) ;
 	    gpio_set_value(toRightButton,GPIO_PIN_VALUE_LOW) ;
 	}
+
 	// Press f to pay respects
 	else if(keyValue == 'f') {
 	    gpio_set_value(toLeftButton,GPIO_PIN_VALUE_HIGH) ;
@@ -99,21 +118,44 @@ int main(int argc, char *argv[]){
 
 	// Press s to stop
 	if(keyValue == 's') {
-	    gpio_set_value(toThrottleButton,GPIO_PIN_VALUE_HIGH) ;
+	    if(throttleStatus == tForward) {
+		throttleStatus = tNeutral ;
+		gpio_set_value(toForwardButton,GPIO_PIN_VALUE_HIGH) ;
+		gpio_set_value(toReverseButton,GPIO_PIN_VALUE_LOW) ;
+	    }
+	    else {
+		gpio_set_value(toReverseButton,GPIO_PIN_VALUE_HIGH) ;
+
+		usleep(100000) ;
+
+		throttleStatus = tReverse;
+		gpio_set_value(toForwardButton,GPIO_PIN_VALUE_HIGH) ;
+		gpio_set_value(toReverseButton,GPIO_PIN_VALUE_LOW) ;
+	    }
 	}
+
 	// Press w to go forward
 	else if(keyValue == 'w') {
-	    gpio_set_value(toThrottleButton,GPIO_PIN_VALUE_LOW) ;
+	    if(throttleStatus == tReverse) {
+		throttleStatus = tNeutral ;
+		gpio_set_value(toForwardButton,GPIO_PIN_VALUE_HIGH) ;
+		gpio_set_value(toReverseButton,GPIO_PIN_VALUE_HIGH) ;
+	    }
+	    else {
+		throttleStatus = tForward;
+		gpio_set_value(toForwardButton,GPIO_PIN_VALUE_LOW) ;
+		gpio_set_value(toReverseButton,GPIO_PIN_VALUE_HIGH) ;
+	    }
 	}
-        // Useful for debugging
-        // cout << "Button " << value << endl;
+
 	usleep(1000);
     }
 
     cout << "\nPhosphophyllite lost her head." << endl;
-    gpio_unexport(toThrottleButton) ;
     gpio_unexport(toLeftButton) ;
-    gpio_unexport(toRightButton) ;    
+    gpio_unexport(toRightButton) ;
+    gpio_unexport(toForwardButton) ;
+    gpio_unexport(toReverseButton) ;
     return 0;
 }
 
