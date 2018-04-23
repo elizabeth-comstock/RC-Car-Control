2ca2d57239aedac54d10432646e836214947d2fd
elizabeth-comstock <36870168+elizabeth-comstock@users.noreply.github.com>
2018-04-23 16:09:49 +0800

    Add files via upload

---
+++ canonicalarduinoread.c
@@ -0,0 +1,101 @@
+/* www.chrisheydrick.com
+     
+   June 23 2012
+
+   CanonicalArduinoRead write a byte to an Arduino, and then
+   receives a serially transmitted string in response.
+
+   The call/response Arduino sketch is here:
+   https://gist.github.com/2980344
+
+   Arduino sketch details at www.chrisheydrick.com
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdint.h>
+#include <fcntl.h>
+#include <termios.h>
+#include <errno.h>
+#include <sys/ioctl.h>
+
+#define DEBUG 1
+
+int main(int argc, char *argv[]) {
+  int fd, n, i;
+  char buf[32] = "temp text";
+  struct termios toptions;
+
+  /* open serial port */
+  fd = open("/dev/ttyACM0", O_RDWR | O_NOCTTY);
+  printf("fd opened as %i\n", fd);
+  
+  /* wait for the Arduino to reboot */
+  usleep(3500000);
+  
+  /* get current serial port settings */
+  tcgetattr(fd, &toptions);
+  /* set 9600 baud both ways */
+  cfsetispeed(&toptions, B9600);
+  cfsetospeed(&toptions, B9600);
+  /* 8 bits, no parity, no stop bits */
+  toptions.c_cflag &= ~PARENB;
+  toptions.c_cflag &= ~CSTOPB;
+  toptions.c_cflag &= ~CSIZE;
+  toptions.c_cflag |= CS8;
+  /* Canonical mode */
+  toptions.c_lflag |= ICANON;
+  /* commit the serial port settings */
+  tcsetattr(fd, TCSANOW, &toptions);
+
+  while(1) {
+
+    // getline function
+    // string input stored at memory location referenced by pointer buffer
+    char *phos;
+    size_t bufsize = 7;
+    size_t characters;
+
+    // bufsize bytes of storage are assigned to memory location buffer 
+    // via malloc() function
+    phos = (char *)malloc(bufsize * sizeof(char));
+
+    // handle the condition where memory isn't available
+    if( phos == NULL)
+    {
+        perror("Unable to allocate buffer. Memory may not be available.");
+        exit(1);
+    }
+
+    printf("Type something: ");
+    characters = getline(&phos,&bufsize,stdin);
+    printf("%zu characters were read.\n",characters);
+    printf("You typed: '%s'\n",phos);
+    // getline function ends here
+
+    // Send byte to trigger Arduino to send string back 
+    write(fd, phos, 7);
+
+    usleep(1000000);
+
+    // Receive string from Arduino 
+    n = read(fd, buf, 7);
+/*
+    // insert terminating zero in the string 
+    buf[n] = 0;
+*/
+    printf("%i bytes read, buffer contains: %s\n", n, buf);
+
+    if(DEBUG) {
+      printf("Printing individual characters in buf as integers...\n\n");
+        for(i=0; i<n; i++) {
+	  printf("Byte %i:%i, ",i+1, (int)buf[i]);
+	}
+      printf("\n");
+    }
+  }
+
+  return 0;
+}
