d954e14e3b591b142b3349954ab9b5bf90ba1263
elizabeth-comstock <36870168+elizabeth-comstock@users.noreply.github.com>
2018-07-09 11:13:29 +0800

    Add files via upload

--- carControlROSNodev2/carControlROSNodev2.ino
+++ carControlROSNodev2/carControlROSNodev2.ino
@@ -1,7 +1,6 @@
 /******************************************************
  Controls steering and throttle servo motors using
  joystick input and ROS. 
-
  Version 2 attempts to add a "stop code", so that the
  car won't continue running if the Arduino, Jetson, or
  joystick suddenly disconnect or encounter an error.
@@ -29,10 +28,15 @@ ros::NodeHandle n;
 Servo steeringServo;
 Servo throttleServo;
 
+int steeringValue = 90;
+int throttleValue = 100;
+
 // time in seconds since last joy message received before automatic stop
 const double noJoyMessageThreshold = 2;
 double timeSinceLastJoyMessage = noJoyMessageThreshold;
 const unsigned long runFrequency = 60;
+
+bool reverseEnabled = 0;
 
 void disconnectCallback(){
   steeringServo.write(90);
@@ -40,8 +44,26 @@ void disconnectCallback(){
 }  
 
 void servoCallback(const std_msgs::UInt32& msg){
-  int throttleValue = msg.data % 1000;
-  int steeringValue = (msg.data - throttleValue) / 1000;
+  throttleValue = msg.data % 1000;
+  steeringValue = (msg.data - throttleValue) / 1000;
+  
+  if (reverseEnabled == 0 && throttleValue >= 110){
+    throttleServo.write(127);
+    delay(500);
+    throttleServo.write(100);
+    delay(500);
+      
+    reverseEnabled = 1;
+  }
+  
+  if (throttleValue <= 95){
+    reverseEnabled = 0;
+  }
+  
+  if (throttleValue >= 103){
+    throttleValue = 103;
+  }
+  
   steeringServo.write(steeringValue);
   throttleServo.write(throttleValue);
   
@@ -68,5 +90,6 @@ void loop(){
     disconnectCallback();
   
   n.spinOnce();
+ 
   delay(1000 / runFrequency);
 }
